30b1aa8545a45fd394188e790bdd799eae6cbb8a
Jianan Ding <jianan.ding1@ucalgary.ca>
2020-03-03 15:18:28 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ function newConnection(socket) {
 
     // change nick name event
     socket.on('change nick name', changeNickName);
+
+    // change nick name color event
+    socket.on('change nick color', changeNickColor);
 }
 
 function getMessage(data) {
@@ -38,26 +41,40 @@ function getMessage(data) {
 }
 
 function init_user(socket) {
-    // check cookie?
-    // if (document.cookie) {
+    socket.emit('has cookie or not');
+    socket.on('has cookie or not', function(data) {
+        console.log("init user socket on ");
         var r = Math.round(Math.random() * 255);
         var g = Math.round(Math.random() * 255);
         var b = Math.round(Math.random() * 255);
-        
-        var num = Math.round(Math.floor(Math.random()*10));
-        var name = fake_name[num];
-        while (current_user.has(name)) {
-            name = fake_name[Math.floor(Math.random()*10)];
-        }
         var color = `rgb(${r}, ${g}, ${b})`
+        var name;
+        if (data.c_name == null) {
+            console.log("client does not have the name cookie");    
+            name = fake_name[Math.round(Math.floor(Math.random()*10))];
+            while (current_user.has(name)) {
+                name = fake_name[Math.floor(Math.random()*10)];
+            }
+        } 
+        else {
+            console.log("client have the name cookie");
+            name = data.c_name;
+            // check if the name exist in the current user now, which mean check the nickname was taken or not
+            if (current_user.has(name)) {
+                name = fake_name[Math.round(Math.floor(Math.random()*10))];
+                while (current_user.has(name)) {
+                    name = fake_name[Math.floor(Math.random()*10)];
+                }
+            }
+        }
         current_user.set(name, color);
         socket.emit('user name', {name, color});
-    // }
+    });
 }
 
 function changeNickName(data) {
-    var new_name = data.new_name;
     var sender = data.sender;
+    var new_name = data.new_name;
     if (!current_user.has(new_name)) {
         var color = current_user.get(sender);
         current_user.delete(sender);
@@ -65,9 +82,17 @@ function changeNickName(data) {
         console.log(current_user);
         // better change this
         io.emit('new user name', {new_name, sender});
-        // need funtion to upadte the displace of current user
     }
     else {
         io.emit('new user name fail', {new_name, sender});
     }
+}
+
+function changeNickColor(data) {
+    var sender = data.sender;
+    var new_color = data.new_color;
+
+    current_user.set(sender, new_color);
+    new_color = current_user.get(sender);
+    io.emit('new user name color', {sender, color: new_color});
 }
