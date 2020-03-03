9f373813c2c30d95fb117ff80419a8c44f7fc889
Jianan Ding <jianan.ding1@ucalgary.ca>
2020-03-02 19:21:25 -0700

    Update

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = 3000;
 
+var current_user = new Map();
+var fake_name = ["Stephen", "James", "David", "Richard", "Kenneth", "Edward", "Olivia", "Emma", "Sophica", "Isabella"];
+
 server.listen(port, function() {
     console.log('Server listerning at port 3000');
 });
@@ -13,15 +16,37 @@ app.use(express.static('public'));
 io.on('connection', newConnection);
 
 function newConnection(socket) {
-    // console.log('new connection: ' + socket.id);
-
+    console.log('new connection: ' + socket.id);
+    var name = init_user();
+    var color = current_user.get(name);
+    socket.emit('user name', {name, color});
     // listen for incoming messgae
     socket.on('chat message', getMessage);
 }
 
-function getMessage(msg) {
+function getMessage(data) {
     var today = new Date();
     var time = today.getHours() > 9? today.getHours() + ":" : "0" + today.getHours() + ":"; 
     time += today.getMinutes() > 9? today.getMinutes(): "0" + today.getMinutes();
-    io.emit('chat message', {msg, time});
+    var sender = data.sender;
+    var msg = data.msg;
+    io.emit('chat message', {sender, msg, time});
+}
+
+function init_user() {
+
+    // if (document.cookie) {
+        var r = Math.random() * 255;
+        var g = Math.random() * 255;
+        var b = Math.random() * 255;
+        
+        var name = fake_name[Math.floor(r*10)];
+        while (current_user.has(name)) {
+            name = fake_name[Math.floor(Math.random()*10)];
+        }
+
+        var color = `rgb(${r}, ${g}, ${b})`
+        current_user.set(name, color);
+        return name;
+    // }
 }
