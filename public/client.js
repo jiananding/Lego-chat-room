13b0dab5edded02d99c9c07fe6a6e70d30eca81d
Jianan Ding <jianan.ding1@ucalgary.ca>
2020-03-08 20:40:11 -0600

    Add files via upload

--- public/client.js
+++ public/client.js
@@ -2,11 +2,9 @@ var socket = io();
 var name;
 var color = "rgb(0, 0, 0)";
 $(function () {
-    // socket.connect('http://localhost:3000');
-
-    $('form').submit(function(e){
+    $('button').click(function(e) {
         e.preventDefault(); // prevents page reloading
-        var msg = $('#m').val();
+        var msg = $('#message-to-send').val();
 
         if (msg.charAt(0) == '/') {
             if (msg.slice(0, 15) == "/nickcolor rgb(") {
@@ -18,49 +16,77 @@ $(function () {
                 socket.emit('change nick name', {sender: name, new_name});
             }
             else {
-                $('#messages').append(`<li><b style="color: white;">Undefine command, please try again.</b></li>`);
+                $('#chat-msg').append(`<li class="clearfix"><div style='font-weight: bold;color: black;'>Undefine command, please try again.</div></li>`);
             }
         }
         else {
             socket.emit('chat message', {msg, sender: name, color});
         }
-        $('#m').val('');
+        $('#message-to-send').val('');
+        $('.chat-history').scrollTop($('.chat-history')[0].scrollHeight);
         return false;
+    });
+
+    $('#message-to-send').bind('keypress', function(e) {
+        if (e.keyCode == 13) {
+            var msg = $('#message-to-send').val();
+
+            if (msg.charAt(0) == '/') {
+                if (msg.slice(0, 15) == "/nickcolor rgb(") {
+                    var new_color = msg.slice(11, msg.length);
+                    socket.emit('change nick color', {sender: name, new_color});
+                }
+                else if (msg.slice(0, 7) == "/nick <"){
+                    var new_name = msg.slice(7, msg.length-1);
+                    socket.emit('change nick name', {sender: name, new_name});
+                }
+                else {
+                    $('#chat-msg').append(`<li class="clearfix"><div style='font-weight: bold;color: black;'>Undefine command, please try again.</div></li>`);
+                }
+            }
+            else {
+                socket.emit('chat message', {msg, sender: name, color});
+            }
+            $('#message-to-send').val('');
+            $('.chat-history').scrollTop($('.chat-history')[0].scrollHeight);
+            return false;
+        }
     });
 
     socket.on('user name', function(data) {
         name = data.name;
         color = data.color;
-        // document.cookie = `name=${name}`;
-        $('#messages').append(`<li><b style='color: white;'>Your are <div id='user_name' style='color:${color}'>${name}</div> now.</b></li>`);
+        document.cookie = `name=${name}`;
+        $('#chat-msg').append(`<li class="clearfix"><div style='font-weight: bold; color: black; text-align: center; font-size: large;'>You are ${data.name} now.</div></li>`);
     });
 
     socket.on('chat message', function(data) {
         if (data.sender != name) {
-            $('#messages').append(`<li><div id='time_stamps'>${data.time}</div> <div id='user_name' style='color:${data.color}'>${data.sender}</div> <div id='msg'>${data.msg}<div/></li>`);
+            $('#chat-msg').append(`<li><div class="message-data"><span class="message-data-name" style="color: ${data.color}">${data.sender}</span><span class="message-data-time"${data.time}</span></div><div class="message other-message">${data.msg}</div></li>`);
         }
         else if (data.sender == name) {
-            $('#messages').append(`<li><div id='time_stamps'>${data.time}</div> <div id='user_name' style='color:${color}'>${data.sender}</div> <b id='msg'>${data.msg}</b></li>`);
+            $('#chat-msg').append(`<li class="clearfix"><div class="message-data align-right"><span class="message-data-time" >${data.time}</span> &nbsp; &nbsp;<span class="message-data-name" style="color: ${data.color}">${data.sender}</span></div><div class="message my-message float-right">${data.msg}</div></li>`);
         }
+        $('.chat-history').scrollTop($('.chat-history')[0].scrollHeight);
     });
 
     socket.on('new user name', function(data) {
         if (name == data.sender) {
             name = data.new_name;
-            $('#messages').append(`<li><b style='color: white;'>You have change your user name to <div id=user_name style='color:${color}'>${name}</div> now.</b></li>`);
+            $('#chat-msg').append(`<li class="clearfix"><div style='font-weight: bold; color: black; text-align: center; font-size: large;'>You are ${name} now.</div></li>`);
         }
     });
 
     socket.on('new user name fail', function(data) {
         if (name == data.sender) {
-            $('#messages').append(`<li><b style='color: white;'>User name <div id=user_name style='color:${color}'>${data.new_name}</div> already exist, please try a new one.</b></li>`);
+            $('#chat-msg').append(`<li class="clearfix"><div style='font-weight: bold;color: black; text-align: center; font-size: large;'>User name <div id=user_name style='color:${color}; display: inline;'>${data.new_name}</div> already exist, please try a new one.</div></li>`);
         }
     });
 
     socket.on('new user name color', function(data) {
         if (name == data.sender) {
             color = data.color;
-            $('#messages').append(`<li><b style='color: white;'>Your user name color has been change to <div id=user_name style='color:${color}'>${data.color}</div>.</b></li>`);
+            $('#chat-msg').append(`<li class="clearfix"><div style='font-weight: bold;color: black; text-align: center; font-size: large;'>Your user name color has been change to <div id=user_name style='color:${color}; display: inline;'>${data.color}</div></li>`);
         }
     });
 
@@ -83,15 +109,20 @@ $(function () {
     }
 
     socket.on('update current user', function(data) {
-        $('#online').empty();
+        $('#online-list').empty();
         for (let i = 0; i < data.all_names.length; i++) {
-            $('#online').append(`<li><div id=user_name style='color:${data.all_color[i]}'>${data.all_names[i]}</div></li>`);
+            if (data.all_names[i] == name) {
+                $('#online-list').append(`<li class="clearfix"><img src="https://image.flaticon.com/icons/svg/1674/1674453.svg" alt="avatar" style="width: 25%; height: auto;"/><div class="about"><div style="color:${data.all_color[i]}">${data.all_names[i]} (you)</div></div></li>`);
+            }
+            else {
+                $('#online-list').append(`<li class="clearfix"><img src="https://image.flaticon.com/icons/svg/1674/1674453.svg" alt="avatar" style="width: 25%; height: auto;"/><div class="about"><div style="color:${data.all_color[i]}">${data.all_names[i]}</div></div></li>`);
+            }
         }
     });
 
     socket.on('upload history', function(log) {
         for (let i = 0; i < log.length; i++) {
-            $('#messages').append(log[i]);
+            $('#chat-msg').append(log[i]);
         }
     });
 });
